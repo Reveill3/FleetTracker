13dae35e43afc62abf5d4ea0963dd77978569257
Reveill3 <38196983+Reveill3@users.noreply.github.com>
2018-05-16 20:37:32 -0500

    Initial
    Initial Work

---
+++ FleetTracker/app.py
@@ -0,0 +1,66 @@
+from flask import (Flask, g, render_template, flash, redirect, url_for,
+                   abort)
+
+from flask_bcrypt import check_password_hash
+from flask_login import (LoginManager, login_user, logout_user,
+                             login_required, current_user)
+
+import forms
+import models
+
+DEBUG = True
+PORT = 8080
+HOST = '172.31.98.152'
+
+app = Flask(__name__)
+app.secret_key = 'auoesh.bouoastuh.43,uoausoehuosth3ououea.auoub!'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+    g.user = current_user
+
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash('Registration Success!', "success")
+        models.User.create_user(username=form.username.data, email=form.email.data, password=form.password.data)
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+if __name__ == '__main__':
+    models.initialize()
+    try:
+        models.User.create_user(
+            username='alester',
+            email='austin.lester@ftsi.com',
+            password='password',
+            admin=True
+        )
+    except ValueError:
+        pass
+    app.run(debug=DEBUG, host=HOST, port=PORT)
