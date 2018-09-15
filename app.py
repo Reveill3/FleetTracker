96d051d220a0548eb0bcec432db200f814d5b056
Reveill3 <38196983+Reveill3@users.noreply.github.com>
2018-09-14 21:10:25 -0500

    added prefixes to forms

--- app.py
+++ app.py
@@ -118,8 +118,8 @@ def register():
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
-    form = forms.LoginForm()
-    search_form = forms.SearchForm()
+    form = forms.LoginForm(prefix='a')
+    search_form = forms.SearchForm(prefix='b')
     if form.validate_on_submit():
         try:
             user_data = search_field('UserName', form.username.data, models.users)
