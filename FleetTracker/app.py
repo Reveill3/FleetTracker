ad7e77ab5bbbf60af25b77da2c491c4e50fa75e4
Reveill3 <38196983+Reveill3@users.noreply.github.com>
2018-10-13 21:51:14 -0500

    feat: changed api calls to heroku

--- FleetTracker/app.py
+++ FleetTracker/app.py
@@ -188,4 +188,4 @@ if __name__ == '__main__':
     #     )
     # except ValueError:
     #     pass
-    app.run(threaded=THREADED, debug=DEBUG, host=HOST, port=PORT)
+    # app.run(threaded=THREADED, debug=DEBUG, host=HOST, port=PORT)
