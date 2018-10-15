ff918fec4ebd9f68478261790e13b33cb0544478
Reveill3 <38196983+Reveill3@users.noreply.github.com>
2018-10-14 23:25:34 -0500

    feat: prev hole hours passed to client

--- resources/get_equipment_list.py
+++ resources/get_equipment_list.py
@@ -52,11 +52,18 @@ class GetEquipmentList(Resource):
                 'station': equipment[2],
                 'maintenance': maint_messages,
                 'movement': move_messages,
+                'pump_hours': pump_hours
                 'hole_1_life': hole_1_life,
                 'hole_2_life': hole_2_life,
                 'hole_3_life': hole_3_life,
                 'hole_4_life': hole_4_life,
-                'hole_5_life': hole_5_life
+                'hole_5_life': hole_5_life,
+                'previous_hours': {'hole_1': int(hole_1_hours[-1]),
+                                    'hole_2': int(hole_2_hours[-1]),
+                                    'hole_3': int(hole_3_hours[-1]),
+                                    'hole_4': int(hole_4_hours[-1]),
+                                    'hole_5': int(hole_5_hours[-1]),
+                                    }
             }
             jsoncollection.append(jsondict)
         sorted_by_station = sorted(jsoncollection, key=lambda k: k['station'])
