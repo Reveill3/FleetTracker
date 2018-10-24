d9ca54df97c86cf4e762c54db2500f06fa5437e2
Reveill3 <38196983+Reveill3@users.noreply.github.com>
2018-10-23 23:18:10 -0500

    feat: note functionality

--- app.py
+++ app.py
@@ -173,17 +173,17 @@ def logout():
     flash('You have been logged out.')
     return redirect(url_for('home'))
 
-if __name__ == '__main__':
-    # models.initialize()
-    # models.initialize_csv()
-    # try:
-    #     models.User.create_user(
-    #         username='alester',
-    #         email='austin.lester@ftsi.com',
-    #         password='password',
-    #         admin=True,
-    #         crew='red'
-    #     )
-    # except ValueError:
-    #     pass
-    app.run(threaded=THREADED, debug=DEBUG, host=HOST, port=PORT)
+# if __name__ == '__main__':
+#     # models.initialize()
+#     # models.initialize_csv()
+#     # try:
+#     #     models.User.create_user(
+#     #         username='alester',
+#     #         email='austin.lester@ftsi.com',
+#     #         password='password',
+#     #         admin=True,
+#     #         crew='red'
+#     #     )
+#     # except ValueError:
+#     #     pass
+#     app.run(threaded=THREADED, debug=DEBUG, host=HOST, port=PORT)
