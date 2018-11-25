ce119b073a863d8e8c446067c6f59eaa5ddf3484
Reveill3 <38196983+Reveill3@users.noreply.github.com>
2018-11-25 14:29:01 -0600

    bug: moved maint_log_list into for loop

--- models.py
+++ models.py
@@ -40,12 +40,12 @@ def create_list(crew, equipment_type):
     equipment_filter = list(filter(lambda e: e['fields']['Type'] == equipment_type, color_filter))
 
     equipment_list = []
-    if len(unit['fields']['Maintenance']) < 10:
-        maint_log_list = unit['fields']['Maintenance']
-    else:
-        maint_log_list = unit['fields']['Maintenance'][-10]
-        
+    
     for unit in equipment_filter:
+            if len(unit['fields']['Maintenance']) < 10:
+                maint_log_list = unit['fields']['Maintenance']
+            else:
+                maint_log_list = unit['fields']['Maintenance'][-10]
         equipment_list.append((unit['fields']['UnitNumber'], unit['fields']['Standby'],
                                unit['fields']['Station'], maint_log_list, unit['fields']['Movement'], unit['fields']['pump_hours'], unit['fields']['Notes']))
     return equipment_list
